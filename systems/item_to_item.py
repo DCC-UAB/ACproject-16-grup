0fb9c28f867ce35ef4a0eb2d9b18506ce7d1b41c
jiacomas <115490765+jiacomas@users.noreply.github.com>
2024-12-01 22:38:57 +0100

    preprocessing (#3)
    * finish preprocessing
    
    * refactor item2item

--- systems/item_to_item.py
+++ systems/item_to_item.py
@@ -7,32 +7,16 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.abspath(os.path.join(script_dir, '..'))
 sys.path.append(parent_dir)
 
-from data_preprocessing.preprocessing_csv import movies_metadata, ratings
+from data_preprocessing.preprocessing_csv import small_ratings
 
 class ItemItemRecommender:
     def __init__(self):
         """
         Inicialitza el sistema de recomanació amb les dades de valoracions i articles.
         """
-        self.ratings = ratings('./datasets/ratings_small.csv')
-        self.items = movies_metadata('./datasets/movies_metadata.csv')
+        self.ratings, self.items = small_ratings()
+        # self.items = movies_metadata()
         self.item_similarity = None
-    
-    # def read_ratings(self, path):
-    #     ratings = pd.read_csv(path)
-    #     ratings = ratings.rename(columns={'userId':'user', 'movieId':'id'})
-    #     ratings.timestamp = pd.to_datetime(ratings.timestamp, unit='s')
-    #     return ratings
-        
-    # def preprocessing_movies(self, path):
-    #     items = pd.read_csv(path)
-    #     items['id'] = pd.to_numeric(items['id'], errors='coerce')
-    #     items = items.dropna(subset=['id'])
-    #     items['id'] = items['id'].astype('int64')
-    #     items['adult'] = items['adult'].map({'True': True, 'False': False})
-    #     items['adult']= items['adult'].astype('bool')
-    #     items = items.convert_dtypes()
-    #     return items
   
     def similarity_matrix_cosine(self):
         """
@@ -58,9 +42,9 @@ class ItemItemRecommender:
         :return: DataFrame amb els articles més similars
         """
         if self.item_similarity is None:
-            if similarity is 'Cosine':
+            if similarity == 'Cosine':
                 self.similarity_matrix_cosine()
-            elif similarity is 'Pearson':
+            elif similarity == 'Pearson':
                 self.similarity_matrix_pearson()
             else:
                 raise ValueError("Métode desconegut: només 'Cosine' o 'Pearson' són vàlids.")
@@ -99,18 +83,18 @@ class ItemItemRecommender:
         
         recommended_items = pd.Series(recommendations).sort_values(ascending=False).head(top_n)
         
-        recommended_items_info = self.items.loc[recommended_items.index]
+        recommended_items_info = self.items.loc[recommended_items.index] # TODO: check error
         recommended_items_info['score'] = recommended_items.values
         
         return recommended_items_info
 
 if __name__ == '__main__':
     user_id = 1
-    # recommender = ItemItemRecommender()
-    # recs_cos = recommender.recommend_for_user(user_id, similarity='Cosine',top_n=3)
-    # print('Recomanacions cosinus:\n',recs_cos)
-    
     recommender = ItemItemRecommender()
-    recs_pear = recommender.recommend_for_user(user_id, similarity='Pearson',top_n=3)
-    print('\nRecomanacions pearson:\n',recs_pear)
+    recs_cos = recommender.recommend_for_user(user_id, similarity='Cosine',top_n=3)
+    print('Recomanacions cosinus:\n',recs_cos)
+    
+    # recommender = ItemItemRecommender()
+    # recs_pear = recommender.recommend_for_user(user_id, similarity='Pearson',top_n=3)
+    # print('\nRecomanacions pearson:\n',recs_pear)
     
