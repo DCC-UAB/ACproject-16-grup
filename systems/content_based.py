204603d3c3912438974d603a124671b574fc6dd3
lukepardo <lukep.729@gmail.com>
2024-12-09 18:04:27 +0100

    main

--- systems/content_based.py
+++ systems/content_based.py
@@ -1,14 +1,21 @@
-from data_preprocessing.preprocessing_csv import small_ratings, keywords
-
+import sys
+import os
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-class ContentBasedRecomender:
+# Ajusta el sistema de directorios para importar correctamente
+script_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(script_dir, '..'))
+sys.path.append(parent_dir)
+
+from data_preprocessing.preprocessing_csv import small_ratings, keywords
+
+class ContentBasedRecommender:
     def __init__(self):
         """
-        Inicialitza el sistema de recomanació amb les dades de valoracions i articles.
+        Inicializa el sistema de recomendación con los datos de valoraciones y películas.
         """
         self.ratings, self.movies = small_ratings()
         self.credits = pd.read_csv("./datasets/credits.csv")
@@ -16,33 +23,32 @@ class ContentBasedRecomender:
         self.cast_similarity = None
         self.keywords_similarity = None
 
-
     def merge_data(self):
         """
-        Merge: Afegeix les keywords, el cast i el crew a les dades de les pel·lícules.
+        Merge: Añade las keywords, el cast y el crew a los datos de las películas.
         """
         key = keywords("./datasets/keywords.csv")
         self.movies = pd.merge(self.movies, key, on='id', how='left')
-        self.movies= self.movies[self.movies['keywords'].apply(lambda x: len(x) > 0)]
-        #no_keywords = self.movies[self.movies['keywords'].apply(lambda x: len(x) == 0)]
+        
+        movies_with_keywords = self.movies[self.movies['keywords'].apply(lambda x: len(x) > 0)]
+        movies_without_keywords = self.movies[self.movies['keywords'].apply(lambda x: len(x) == 0)]
+        
         self.movies = pd.merge(self.movies, self.credits, on='id', how='left')
+        return movies_with_keywords, movies_without_keywords
 
 
     def tfidf(self):
         """
-        Crea una matriu TF-IDF per a un conjunt de dades.
-        :param column: Nom de la columna per a la qual es vol crear la matriu.
-        :return: Matriu TF-IDF
+        Crea una matriz TF-IDF para el contenido de las películas basado en la columna 'overview'.
         """
         tfidf = TfidfVectorizer(stop_words='english')
         self.movies['overview'] = self.movies['overview'].fillna('')
         tfidf_matrix = tfidf.fit_transform(self.movies["overview"])
-        
         return tfidf_matrix
-    
+
     def find_similar(self, title, n, sim):
         """
-        Funció per recomanar utilitzant el coeficient de Pearson.
+        Función para recomendar utilizando una métrica de similitud.
         """
         index = pd.Series(self.movies.index, index=self.movies.title).drop_duplicates()
         idx = index[title]
@@ -53,17 +59,20 @@ class ContentBasedRecomender:
 
 
 if __name__ == '__main__':
-    recomender = ContentBasedRecomender()
-    recomender.merge_data()
-    tfidf_matrix = recomender.tfidf()
-    
-    #Coficient de Pearson
+    recommender = ContentBasedRecommender()    
+    movies_with_keywords, movies_without_keywords = recommender.merge_data()
+    print(movies_with_keywords["overview"].head())
+    tfidf_matrix = recommender.tfidf()
+
+    # Similitud amb Pearson
     tfidf_array = tfidf_matrix.toarray()
     pearson_sim = np.corrcoef(tfidf_array)
-    r=recomender.find_similar("Casino", 10, pearson_sim)
-    print(r,'\n')
-    
-    #Distància cosinus
+    print("Recomendaciones con Pearson:")
+    recommendations_pearson = recommender.find_similar("Casino", 10, pearson_sim)
+    print(recommendations_pearson, '\n')
+
+    # Similitud amb coseno
     cosine_sim = linear_kernel(tfidf_matrix)
-    r=recomender.find_similar("Casino", 10, cosine_sim)
-    print(r,'\n')
+    print("Recomendaciones con Coseno:")
+    recommendations_cosine = recommender.find_similar("Casino", 10, cosine_sim)
+    print(recommendations_cosine, '\n')
