a46b84a5cbfe37f07ccd6cbc7c03fda423edcaba
jiacomas <jcomas.fr@gmail.com>
2024-12-09 18:11:40 +0100

    merge

--- data_preprocessing/metriques.py
+++ data_preprocessing/metriques.py
@@ -15,7 +15,7 @@ class preprocessing:
         self.credits = pd.read_csv("./datasets/credits.csv")
         self.movies = movies_metadata("./datasets/movies_metadata.csv")
         # self.preprocessing_data()
-        self.movies = self.movies[self.movies['id'].isin(self.credits['id'])]
+        self.movies = self.movies[self.movies["id"].isin(self.credits["id"])]
         # self.keywords = pd.read_csv("./datasets/keywords.csv")
         # self.links = pd.read_csv("./datasets/links.csv")
 
@@ -25,151 +25,190 @@ class preprocessing:
     #     self.movies['id'] = self.movies['id'].astype('int64')
     #     self.movies = self.movies.convert_dtypes()
     #     return self.movies
-        
-    # Funció per mostrar la distribució de les puntuacions    
+
+    # Funció per mostrar la distribució de les puntuacions
     def display_rating_distribution(self):
         plt.figure(figsize=(10, 6))
-        self.ratings['rating'].hist(bins=20, edgecolor='black')
-        plt.title('Distribució de Puntuacions')
-        plt.xlabel('Puntuació')
-        plt.ylabel('Nombre de Vots')
+        self.ratings["rating"].hist(bins=20, edgecolor="black")
+        plt.title("Distribució de Puntuacions")
+        plt.xlabel("Puntuació")
+        plt.ylabel("Nombre de Vots")
         plt.grid(False)
-        plt.gca().yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{int(x):,}'))
+        plt.gca().yaxis.set_major_formatter(
+            plt.FuncFormatter(lambda x, _: f"{int(x):,}")
+        )
         plt.show()
 
     # Funció per a analitzar les mètriques de les pel·lícules
     def movie_metrics(self, min_vots):
         print(f"Total de pel·lícules: {self.movies.shape[0]}\n")
         print(f"Valors nulls a les pel·lícules:\n{self.movies.isnull().sum()}\n")
-        print(f"Pel·lícules més votades:\n{self.movies.sort_values('vote_count', ascending=False)[['id','title', 'vote_count', 'vote_average']].head(10)}\n")
-        print(f"Pel·lícules amb millor puntuació i més de {min_vots} vots:\n{self.movies[self.movies['vote_count'] > min_vots].sort_values('vote_average', ascending=False)[['id','title', 'vote_count', 'vote_average']].head(10)}\n")
+        print(
+            f"Pel·lícules més votades:\n{self.movies.sort_values('vote_count', ascending=False)[['id','title', 'vote_count', 'vote_average']].head(10)}\n"
+        )
+        print(
+            f"Pel·lícules amb millor puntuació i més de {min_vots} vots:\n{self.movies[self.movies['vote_count'] > min_vots].sort_values('vote_average', ascending=False)[['id','title', 'vote_count', 'vote_average']].head(10)}\n"
+        )
 
     # Funció per mostrar les pel·lícules més valorades segons la puntuació ponderada
     def top_rated_movies(self):
-        C = self.movies['vote_average'].mean()
-        m = self.movies['vote_count'].quantile(0.3)
-        self.movies["puntuacio_ponderada"] = (self.movies['vote_count']*self.movies['vote_average'] + C*m) / (self.movies['vote_count'] + m)
-        top20 = self.movies.sort_values('puntuacio_ponderada', ascending=False).drop_duplicates('title').head(20)
-        sns.barplot(x='puntuacio_ponderada', y='title', data=top20, palette='viridis')
-        plt.title('Top 20 Pel·lícules per Puntuació Ponderada', fontsize=16)
-        plt.xlabel('Puntuació Ponderada (PP)', fontsize=12)
-        plt.ylabel('Títol de la Pel·lícula', fontsize=12)
-        min_pp = top20['puntuacio_ponderada'].min()
-        max_pp = top20['puntuacio_ponderada'].max()
+        C = self.movies["vote_average"].mean()
+        m = self.movies["vote_count"].quantile(0.3)
+        self.movies["puntuacio_ponderada"] = (
+            self.movies["vote_count"] * self.movies["vote_average"] + C * m
+        ) / (self.movies["vote_count"] + m)
+        top20 = (
+            self.movies.sort_values("puntuacio_ponderada", ascending=False)
+            .drop_duplicates("title")
+            .head(20)
+        )
+        sns.barplot(x="puntuacio_ponderada", y="title", data=top20, palette="viridis")
+        plt.title("Top 20 Pel·lícules per Puntuació Ponderada", fontsize=16)
+        plt.xlabel("Puntuació Ponderada (PP)", fontsize=12)
+        plt.ylabel("Títol de la Pel·lícula", fontsize=12)
+        min_pp = top20["puntuacio_ponderada"].min()
+        max_pp = top20["puntuacio_ponderada"].max()
         plt.xlim(min_pp - 0.1, max_pp + 0.1)
-        for index, value in enumerate(top20['puntuacio_ponderada']):
-            plt.text(value - 0.001, index, f'{value:.3f}', color='black', va="center")
+        for index, value in enumerate(top20["puntuacio_ponderada"]):
+            plt.text(value - 0.001, index, f"{value:.3f}", color="black", va="center")
 
-        plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter('%.1f'))
+        plt.gca().xaxis.set_major_formatter(ticker.FormatStrFormatter("%.1f"))
         plt.show()
-    
+
     # Funció per analitzar els gèneres de les pel·lícules
     def analitza_generes(self):
         self.movies['genres'] = self.movies['genres'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
         tots_generes = self.movies['genres'].explode()
         generes = tots_generes.value_counts().reset_index()
-        generes.columns = ['gènere', 'comptador']
+        generes.columns = ["gènere", "comptador"]
         plt.figure(figsize=(10, 6))
-        sns.barplot(x='comptador', y='gènere', data=generes, palette='viridis')
-        plt.title('Distribució de Gèneres')
-        plt.xlabel('Nombre de Pel·lícules')
-        plt.ylabel('Gènere')
+        sns.barplot(x="comptador", y="gènere", data=generes, palette="viridis")
+        plt.title("Distribució de Gèneres")
+        plt.xlabel("Nombre de Pel·lícules")
+        plt.ylabel("Gènere")
         plt.show()
 
     # Funció per comptar pel·lícules per intervals de durada
     def movies_runtime_distribution(self):
-        mv = self.movies.dropna(subset=['runtime', 'popularity'])
-        mv['popularity'] = mv['popularity'].astype(float)
+        mv = self.movies.dropna(subset=["runtime", "popularity"])
+        mv["popularity"] = mv["popularity"].astype(float)
         intervals = [0, 30, 80, 130, np.inf]
-        etiquetes = ['Curtmetratge', 'Pel·lícula curta', 'Pel·lícula', 'Pel·lícula llarga']
-        runtime = pd.to_numeric(mv['runtime'], errors='coerce')
+        etiquetes = [
+            "Curtmetratge",
+            "Pel·lícula curta",
+            "Pel·lícula",
+            "Pel·lícula llarga",
+        ]
+        runtime = pd.to_numeric(mv["runtime"], errors="coerce")
         durada = pd.cut(runtime, bins=intervals, labels=etiquetes, right=False)
         comptador_intervals = durada.value_counts().sort_index()
-        popularitat_intervals = mv.groupby(durada)['popularity'].mean()
-        popularitat_intervals = mv.groupby(durada)['popularity'].mean()
-        taula_intervals = pd.DataFrame({'Quantitat': comptador_intervals,'Popularitat Mitjana': popularitat_intervals})
+        popularitat_intervals = mv.groupby(durada)["popularity"].mean()
+        popularitat_intervals = mv.groupby(durada)["popularity"].mean()
+        taula_intervals = pd.DataFrame(
+            {
+                "Quantitat": comptador_intervals,
+                "Popularitat Mitjana": popularitat_intervals,
+            }
+        )
         print(taula_intervals)
-
 
     # Funció per mirar la relació entre la puntuació i el nombre de vots
     def vote_vs_rate(self):
-        correlacio = self.movies[['vote_count', 'vote_average']].corr().iloc[0, 1]
+        correlacio = self.movies[["vote_count", "vote_average"]].corr().iloc[0, 1]
         print(f"Correlació entre Nombre de Vots i Puntuació: {correlacio:.2f}")
         plt.figure(figsize=(10, 6))
-        sns.scatterplot(x='vote_count', y='vote_average', data=self.movies, alpha=0.7)
-        plt.title('Puntuació vs Nombre de Vots')
-        plt.xlabel('Nombre de Vots')
-        plt.ylabel('Puntuació')
+        sns.scatterplot(x="vote_count", y="vote_average", data=self.movies, alpha=0.7)
+        plt.title("Puntuació vs Nombre de Vots")
+        plt.xlabel("Nombre de Vots")
+        plt.ylabel("Puntuació")
         plt.show()
 
-    # Funció per analitzar si els directors i actors més populars influeixen en la popularitat de les pel·lícules 
+    # Funció per analitzar si els directors i actors més populars influeixen en la popularitat de les pel·lícules
     def get_most_popular_cast_crew(self):
-        crew = self.credits['crew'].apply(literal_eval)
-        cast = self.credits['cast'].apply(literal_eval)
+        crew = self.credits["crew"].apply(literal_eval)
+        cast = self.credits["cast"].apply(literal_eval)
 
-        self.credits['directors'] = crew.apply(lambda x: [i['name'] for i in x if i['job'] == 'Director'])
-        directors = self.credits.explode('directors')[['id', 'directors']].dropna(subset=['directors'])
+        self.credits["directors"] = crew.apply(
+            lambda x: [i["name"] for i in x if i["job"] == "Director"]
+        )
+        directors = self.credits.explode("directors")[["id", "directors"]].dropna(
+            subset=["directors"]
+        )
 
-        directors_counts = directors['directors'].value_counts().reset_index()
-        directors_counts.columns = ['Director', 'Nombre de Pel·lícules']
+        directors_counts = directors["directors"].value_counts().reset_index()
+        directors_counts.columns = ["Director", "Nombre de Pel·lícules"]
 
-        directors_ids = directors.groupby('directors')['id'].apply(list).reset_index()
-        directors_ids.columns = ['Director', 'id']
+        directors_ids = directors.groupby("directors")["id"].apply(list).reset_index()
+        directors_ids.columns = ["Director", "id"]
 
-        directors_final = pd.merge(directors_counts, directors_ids, on='Director')
+        directors_final = pd.merge(directors_counts, directors_ids, on="Director")
         top10_directors = directors_final.head(10)
-        
-        
-        self.credits['actors'] = cast.apply(lambda x: [i['name'] for i in x])
-        actors = self.credits.explode('actors')[['id', 'actors']].dropna(subset=['actors'])
 
-        actors_counts = actors['actors'].value_counts().reset_index()
-        actors_counts.columns = ['Actor', 'Nombre de Pel·lícules']
+        self.credits["actors"] = cast.apply(lambda x: [i["name"] for i in x])
+        actors = self.credits.explode("actors")[["id", "actors"]].dropna(
+            subset=["actors"]
+        )
 
-        actors_ids = actors.groupby('actors')['id'].apply(list).reset_index()
-        actors_ids.columns = ['Actor', 'id']
+        actors_counts = actors["actors"].value_counts().reset_index()
+        actors_counts.columns = ["Actor", "Nombre de Pel·lícules"]
 
-        actors_final = pd.merge(actors_counts, actors_ids, on='Actor')
+        actors_ids = actors.groupby("actors")["id"].apply(list).reset_index()
+        actors_ids.columns = ["Actor", "id"]
+
+        actors_final = pd.merge(actors_counts, actors_ids, on="Actor")
         top10_actors = actors_final.head(10)
-        
-        print(f"Actors més influents:\n{top10_actors[['Actor','Nombre de Pel·lícules']]}\n")
-        print(f"Directors més influents:\n{top10_directors[['Director', 'Nombre de Pel·lícules']]}\n")
-        
+
+        print(
+            f"Actors més influents:\n{top10_actors[['Actor','Nombre de Pel·lícules']]}\n"
+        )
+        print(
+            f"Directors més influents:\n{top10_directors[['Director', 'Nombre de Pel·lícules']]}\n"
+        )
+
         dins_millors_pelis = {}
-        top250 = self.movies.sort_values('puntuacio_ponderada', ascending=False).drop_duplicates('title').head(250)
-        for (_, actor_row), (_, director_row) in zip(top10_actors.iterrows(), top10_directors.iterrows()):
-            dins_millors_pelis[actor_row['Actor']] = ['actor', 0]
-            dins_millors_pelis[director_row['Director']] = ['director', 0]
-            for peli_actor in actor_row['id']:
-                if peli_actor in top250['id'].values:  
-                    dins_millors_pelis[actor_row['Actor']][1] += 1
-            for peli_director in director_row['id']:
-                if peli_director in top250['id'].values:
-                    dins_millors_pelis[director_row['Director']][1] += 1
-        
-        data = {'Nom': [], 'Rol': [], 'Comptador': []}
+        top250 = (
+            self.movies.sort_values("puntuacio_ponderada", ascending=False)
+            .drop_duplicates("title")
+            .head(250)
+        )
+        for (_, actor_row), (_, director_row) in zip(
+            top10_actors.iterrows(), top10_directors.iterrows()
+        ):
+            dins_millors_pelis[actor_row["Actor"]] = ["actor", 0]
+            dins_millors_pelis[director_row["Director"]] = ["director", 0]
+            for peli_actor in actor_row["id"]:
+                if peli_actor in top250["id"].values:
+                    dins_millors_pelis[actor_row["Actor"]][1] += 1
+            for peli_director in director_row["id"]:
+                if peli_director in top250["id"].values:
+                    dins_millors_pelis[director_row["Director"]][1] += 1
+
+        data = {"Nom": [], "Rol": [], "Comptador": []}
         for nom, (rol, comptador) in dins_millors_pelis.items():
-            data['Nom'].append(nom)
-            data['Rol'].append(rol)
-            data['Comptador'].append(comptador)
+            data["Nom"].append(nom)
+            data["Rol"].append(rol)
+            data["Comptador"].append(comptador)
 
         df = pd.DataFrame(data)
 
         # Gràfic de barres
         plt.figure(figsize=(10, 6))
-        sns.barplot(x='Comptador', y='Nom', hue='Rol', data=df, palette='viridis')
-        plt.title('Actors i Directors més repetits a les 250 pel·lícules més votades', fontsize=16)
-        plt.xlabel('Nombre de Vegades a les 250 Millors Pel·lícules', fontsize=12)
-        plt.ylabel('Nom', fontsize=12)
-        plt.legend(title='Rol', loc='upper right')
+        sns.barplot(x="Comptador", y="Nom", hue="Rol", data=df, palette="viridis")
+        plt.title(
+            "Actors i Directors més repetits a les 250 pel·lícules més votades",
+            fontsize=16,
+        )
+        plt.xlabel("Nombre de Vegades a les 250 Millors Pel·lícules", fontsize=12)
+        plt.ylabel("Nom", fontsize=12)
+        plt.legend(title="Rol", loc="upper right")
         plt.show()
 
-    
-if __name__=="__main__":
+
+if __name__ == "__main__":
     p = preprocessing()
     p.display_rating_distribution()
-    p.movie_metrics(1000) 
-    
+    p.movie_metrics(1000)
+
     p.top_rated_movies()
     p.analitza_generes()
     p.movies_runtime_distribution()
--- data_preprocessing/preprocessing_csv.py
+++ data_preprocessing/preprocessing_csv.py
@@ -2,129 +2,150 @@ import pandas as pd
 import ast
 import time
 
-PATH_MOVIES = './datasets/movies_metadata.csv'
-PATH_RATINGS = './datasets/ratings.csv'
-PATH_RATINGS_SMALL = './datasets/ratings_small.csv'
-PATH_CREDITS = './datasets/credits.csv'
-PATH_KEYWORDS = './datasets/keywords.csv'
-PATH_LINKS = './datasets/links.csv'
-PATH_LINKS_SMALL = './datasets/links_small.csv'
+PATH_MOVIES = "./datasets/movies_metadata.csv"
+PATH_RATINGS = "./datasets/ratings.csv"
+PATH_RATINGS_SMALL = "./datasets/ratings_small.csv"
+PATH_CREDITS = "./datasets/credits.csv"
+PATH_KEYWORDS = "./datasets/keywords.csv"
+PATH_LINKS = "./datasets/links.csv"
+PATH_LINKS_SMALL = "./datasets/links_small.csv"
 
-ID = 'id'
+ID = "id"
+
 
 def movies_metadata(path=PATH_MOVIES):
-        items = pd.read_csv(path, low_memory=False)
-        items[ID] = pd.to_numeric(items[ID], errors='coerce')
-        items = items.dropna(subset=[ID])
-        items[ID] = items[ID].astype('int64')
-        
-        items['adult'] = items['adult'].map({'True': True, 'False': False})
-        items['adult']= items['adult'].astype('bool')
-        
-        items['belongs_to_collection'] = items['belongs_to_collection'].fillna('{}')
-        items['belongs_to_collection'] = items['belongs_to_collection'].apply(ast.literal_eval)
-        items['belongs_to_collection'] = items['belongs_to_collection'].apply(lambda x: None if x == {} else x)
-        
-        # TODO: genres, production_companies, production_countries, spoken_languages
-        items['genres'] = items['genres'].fillna('[]').apply(ast.literal_eval)
-        items['production_companies'] = items['production_companies'].fillna('[]').apply(ast.literal_eval)
-        items['production_countries'] = items['production_countries'].fillna('[]').apply(ast.literal_eval)
-        items['spoken_languages'] = items['spoken_languages'].fillna('[]').apply(ast.literal_eval)
-        
-        items = items.convert_dtypes()
-        return items
-    
+    items = pd.read_csv(path, low_memory=False)
+    items[ID] = pd.to_numeric(items[ID], errors="coerce")
+    items = items.dropna(subset=[ID])
+    items[ID] = items[ID].astype("int64")
+
+    items["adult"] = items["adult"].map({"True": True, "False": False})
+    items["adult"] = items["adult"].astype("bool")
+
+    items["belongs_to_collection"] = items["belongs_to_collection"].fillna("{}")
+    items["belongs_to_collection"] = items["belongs_to_collection"].apply(
+        ast.literal_eval
+    )
+    items["belongs_to_collection"] = items["belongs_to_collection"].apply(
+        lambda x: None if x == {} else x
+    )
+
+    # TODO: genres, production_companies, production_countries, spoken_languages
+    items["genres"] = (
+        items["genres"]
+        .fillna("[]")
+        .apply(ast.literal_eval)
+        .apply(lambda x: None if x == {} else x)
+    )
+    items["production_companies"] = (
+        items["production_companies"].fillna("[]").apply(ast.literal_eval)
+    )
+    items["production_countries"] = (
+        items["production_countries"].fillna("[]").apply(ast.literal_eval)
+    )
+    items["spoken_languages"] = (
+        items["spoken_languages"].fillna("[]").apply(ast.literal_eval)
+    )
+
+    items = items.convert_dtypes()
+    return items
+
+
 def data_ratings(path=PATH_RATINGS):
-        ratings = pd.read_csv(path)
-        ratings = ratings.rename(columns={'userId':'user', 'movieId':ID})
-        ratings.timestamp = pd.to_datetime(ratings.timestamp, unit='s')
-        return ratings
+    ratings = pd.read_csv(path)
+    ratings = ratings.rename(columns={"userId": "user", "movieId": ID})
+    ratings.timestamp = pd.to_datetime(ratings.timestamp, unit="s")
+    return ratings
+
 
 # Funció per convertir la cadena JSON en una llista de diccionaris
 def convert_to_dict_list(string):
-        return ast.literal_eval(string)
+    return ast.literal_eval(string)
+
 
 def credits(path, path_movies=PATH_MOVIES):
-        movies = movies_metadata(path_movies)
-        credits = pd.read_csv(path)
+    movies = movies_metadata(path_movies)
+    credits = pd.read_csv(path)
 
-        id_movies = set(movies[ID])
+    id_movies = set(movies[ID])
 
-        credits = credits[credits[ID].isin(id_movies)]
+    credits = credits[credits[ID].isin(id_movies)]
 
-        credits['cast'] = credits['cast'].apply(convert_to_dict_list)
-        credits['crew'] = credits['crew'].apply(convert_to_dict_list)
+    credits["cast"] = credits["cast"].apply(convert_to_dict_list)
+    credits["crew"] = credits["crew"].apply(convert_to_dict_list)
 
-        cast_rows = []
-        crew_rows = []
+    cast_rows = []
+    crew_rows = []
 
-        for _,row in credits.iterrows():
-                for cast_member in row.cast:
-                        cast_member['movie_id'] = row[ID]
-                        cast_rows.append(cast_member)
-                
-                for crew_member in row.crew:
-                        crew_member['movie_id'] = row[ID]
-                        crew_rows.append(crew_member)
+    for _, row in credits.iterrows():
+        for cast_member in row.cast:
+            cast_member["movie_id"] = row[ID]
+            cast_rows.append(cast_member)
 
-        cast = pd.DataFrame(cast_rows)
-        crew = pd.DataFrame(crew_rows)
-        
-        return cast.convert_dtypes(), crew.convert_dtypes()
+        for crew_member in row.crew:
+            crew_member["movie_id"] = row[ID]
+            crew_rows.append(crew_member)
+
+    cast = pd.DataFrame(cast_rows)
+    crew = pd.DataFrame(crew_rows)
+
+    return cast.convert_dtypes(), crew.convert_dtypes()
+
 
 def keywords(path=PATH_KEYWORDS):
-        keywords = pd.read_csv(path)
-        keywords['keywords'] = keywords['keywords'].apply(convert_to_dict_list)
-        keywords['keywords_id'] = keywords['keywords'].apply(lambda x: [d['id'] for d in x])
-        keywords['keywords'] = keywords['keywords'].apply(lambda x: [d['name'] for d in x])
-        return keywords
+    keywords = pd.read_csv(path)
+    keywords["keywords"] = keywords["keywords"].apply(convert_to_dict_list)
+    keywords["keywords_id"] = keywords["keywords"].apply(lambda x: [d["id"] for d in x])
+    keywords["keywords"] = keywords["keywords"].apply(lambda x: [d["name"] for d in x])
+    return keywords
+
 
 def links(path=PATH_LINKS):
-        return pd.read_csv(path)
+    return pd.read_csv(path)
+
 
 def small_ratings():
-        ratings = data_ratings(PATH_RATINGS_SMALL)
-        movies = movies_metadata(PATH_MOVIES)
-        movies = movies[movies[ID].isin(ratings[ID])]
-        return ratings, movies
+    ratings = data_ratings(PATH_RATINGS_SMALL)
+    movies = movies_metadata(PATH_MOVIES)
+    movies = movies[movies[ID].isin(ratings[ID])]
+    return ratings, movies
 
 
+if __name__ == "__main__":
+    # cast, crew = credits(PATH_CREDITS, PATH_MOVIES)
+    # keywords = keywords(PATH_KEYWORDS)
+    # links = links(PATH_LINKS)
+    # links_small = links(PATH_LINKS_SMALL)
+    # movies = movies_metadata(PATH_MOVIES)
+    # ratings = data_ratings(PATH_RATINGS)
+    ratings_small = small_ratings()
 
-if __name__ == '__main__':
-        # cast, crew = credits(PATH_CREDITS, PATH_MOVIES)
-        # keywords = keywords(PATH_KEYWORDS)
-        # links = links(PATH_LINKS)
-        # links_small = links(PATH_LINKS_SMALL)
-        # movies = movies_metadata(PATH_MOVIES)
-        # ratings = data_ratings(PATH_RATINGS)
-        ratings_small = small_ratings()
+    # print('CREDITS\n')
+    # print(cast.info())
+    # time.sleep(5)
+    # print(crew.info())
+    # time.sleep(5)
 
-        # print('CREDITS\n')
-        # print(cast.info())
-        # time.sleep(5)
-        # print(crew.info())
-        # time.sleep(5)
+    # print('\n\nKEYWORDS\n',)
+    # print(keywords.info())
+    # time.sleep(5)
 
-        # print('\n\nKEYWORDS\n',)
-        # print(keywords.info())
-        # time.sleep(5)
+    # print('\n\nLINKS\n')
+    # print(links.info())
+    # time.sleep(5)
 
-        # print('\n\nLINKS\n')
-        # print(links.info())
-        # time.sleep(5)
+    # print('\n\nLINKS_SMALL\n',)
+    # print(links_small.info())
+    # time.sleep(5)
 
-        # print('\n\nLINKS_SMALL\n',)
-        # print(links_small.info())
-        # time.sleep(5)
+    # print('\n\nMOVIES\n')
+    # print(movies.info())
+    # time.sleep(5)
 
-        # print('\n\nMOVIES\n')
-        # print(movies.info())
-        # time.sleep(5)
+    # print('\n\nRATINGS\n')
+    # print(ratings.info())
+    # time.sleep(5)
 
-        # print('\n\nRATINGS\n')
-        # print(ratings.info())
-        # time.sleep(5)
-
-        # print('\n\nRATINGS_SMALL\n')
-        # print(ratings_small.info())
-        # time.sleep(5)
+    # print('\n\nRATINGS_SMALL\n')
+    # print(ratings_small.info())
+    # time.sleep(5)
