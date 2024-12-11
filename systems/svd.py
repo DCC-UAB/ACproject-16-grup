353c858435bb7c8364da40049c13c85b26831081
Lluc03 <llucverdaguermacias@gmail.com>
2024-12-11 11:44:05 +0100

    millor n_factor, svd

--- systems/svd.py
+++ systems/svd.py
@@ -2,6 +2,8 @@ from surprise import SVD
 from surprise import Dataset, Reader, accuracy
 from surprise.model_selection import train_test_split
 import pandas as pd
+import matplotlib.pyplot as plt
+
 class SVDRecommender:
     def __init__(self, model, ratings, data):
         self.model = model
@@ -9,10 +11,9 @@ class SVDRecommender:
         self.data = data
         self.train, self.test = train_test_split(data, test_size=0.2, random_state=42)
 
-        
     def train_model(self):
         self.model.fit(self.train)
-        predictions = model.test(self.test)
+        predictions = self.model.test(self.test)
         return predictions
     
     def recommend_for_user(self, user_id, top_n=5):
@@ -28,12 +29,45 @@ if __name__ == '__main__':
     ratings = pd.read_csv("./datasets/ratings_small.csv")
     reader = Reader(rating_scale=(0.5, 5)) 
     data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
+    
+    #Llista per n_factors
+    n_factors_list = [10, 20, 50, 100, 150, 200]
+    rmse_values = []
+
+    #Bucle per provar diferents n_factors
+    for n in n_factors_list:
+        model = SVD(n_factors=n, random_state=42)
+        svd = SVDRecommender(model, ratings, data)
+
+        predictions = svd.train_model()
+        rmse = accuracy.rmse(predictions, verbose=False)
+        rmse_values.append(rmse)
+        print(f"n_factors: {n}, RMSE: {rmse:.4f}")
+
+    #Gràfica
+    plt.figure(figsize=(8, 6))
+    plt.plot(n_factors_list, rmse_values, marker='o', linestyle='-', color='r')
+    plt.title("n_factor que s'ajusta més", fontsize = 14)
+    plt.xlabel("n_factors", fontsize = 12)
+    plt.ylabel("RMSE",fontsize = 12)
+    plt.grid(True)
+    plt.show
+
+    #Més Baix: n_factor = 10
+    #Baix, però agafa relacions més complexes: n_factor = 50
+    '''
+    -> Escull n_factors=10 si busques un model més senzill, 
+    amb un rendiment lleugerament millor (menor RMSE).
+
+    -> Escull n_factors=50 si el lleuger augment en el RMSE no és una preocupació 
+    i vols explorar un model més expressiu i potent per capturar relacions més complexes.
+    '''
     model = SVD(n_factors=50, random_state=42) 
     svd=SVDRecommender(model, ratings, data)
 
     predictions = svd.train_model()
-    mse = accuracy.mse(predictions)
-    print(f"Root Mean Squared Error (MSE): {mse:.4f}")
+    rmse = accuracy.rmse(predictions)
+    print(f"Root Mean Squared Error (RMSE): {rmse:.4f}")
 
 
     recomendations=svd.recommend_for_user(10, 5)
